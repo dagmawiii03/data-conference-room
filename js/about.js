a514f317f503c7b0ad85c3b4429bb09dadea8e4f
dagmawiii03 <dagmawi.yoha03@gmail.com>
2023-03-23 18:52:22 +0300

    fix eslint errros

--- js/about.js
+++ js/about.js
@@ -2,26 +2,27 @@ window.onload = () => {
   const hamburger = document.querySelector('.menu1');
   const closeBtn = document.querySelector('.closeBtn');
   hamburger.addEventListener('click', () => {
-    console.log("clicked")
+    /* eslint-disable no-console */
+    console.log('clicked')
     const menu = document.querySelector('.mobile-menu');
     menu.style.display = 'block';
   });
-  function close(menu, toBeClose){
-    menu.addEventListener('click' , () =>{
-    const value = document.querySelector(toBeClose);
-    value.style.display = 'none';
-    });   
-  };  
-  close(closeBtn,'.mobile-menu');
+  function close(menu, toBeClose) {
+    menu.addEventListener('click' , () => {
+      const value = document.querySelector(toBeClose);
+      value.style.display = 'none';
+    });
+  }
+  close(closeBtn, '.mobile-menu');
   closeBtn.addEventListener('click', () => {
     const menu = document.querySelector('.mobile-menu');
     menu.style.display = 'none';
     close(menu);
   });
   const backToMainPage = document.querySelector('.menuPopUp');
-  close(backToMainPage,'.mobile-menu');
+  close(backToMainPage, '.mobile-menu');
   backToMainPage.addEventListener('click', () => {
     const menu = document.querySelector('.mobile-menu');
     menu.style.display = 'none';
-   });  
+  }); 
 };
